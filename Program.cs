6528d7307fdc8c51e1d976cef60c35d54af9af04
Mariana S. Mz <mscmazi@gmail.com>
2020-10-07 16:17:52 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace LaserOpDeleter
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new LaserOpDeleter()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ Service1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Timers;
+
+namespace LaserOpDeleter
+{
+    public partial class LaserOpDeleter : ServiceBase
+    {
+        public LaserOpDeleter()
+        {
+            InitializeComponent();
+        }
+
+        public void FileRemover()
+        {
+            // Add the path for the directory
+            string FilePath = @"C:\MyPath";
+            try
+            {
+                // Create an array for all the files contained in the directory
+                string[] laserFiles = Directory.GetFiles(FilePath);
+                for (int i = 0; i < laserFiles.Length; i++)
+                {
+                    //Delete all files that are older than 30 days
+                    if (DateTime.Now.Subtract(File.GetLastWriteTime(laserFiles[i])).TotalDays > 30)
+                    {
+                        File.Delete(laserFiles[i]);
+                    }
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        public void OnTimer(object sender, ElapsedEventArgs args)
+        {
+            // To do actions: 
+            FileRemover();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Timer timer = new Timer(); // Set a timer to monitor the system
+            timer.Interval = 60000; // Every 60 secs
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(this.OnTimer); // Create a new handler
+            // Timer is enabled and started
+            timer.Enabled = true; 
+            timer.Start();
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
